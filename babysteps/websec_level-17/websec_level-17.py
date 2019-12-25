45bf843ae9bf0b988b063be9aa3a8182aca5e755
entroy0421 <39701965a@gmail.com>
2019-12-26 00:38:50 +0800

    how to solved

--- babysteps/websec_level-17/websec_level-17.py
+++ babysteps/websec_level-17/websec_level-17.py
@@ -7,4 +7,11 @@ data = {
 }
 
 r = post(url , data=data)
-print r.text
+pos = r.text.find("WEBSEC{")
+while (True):
+    txt = r.text[pos]
+    print(txt, end='')
+    if(txt == '}'):
+        print()
+        break
+    pos += 1
