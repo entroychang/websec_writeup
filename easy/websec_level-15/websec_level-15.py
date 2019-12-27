444daa2fbda4b21c4345be2ebff8ac5f630b639a
entroy <56551382+entroy0421@users.noreply.github.com>
2019-12-27 21:40:49 +0800

    Update websec_level-15.py

--- easy/websec_level-15/websec_level-15.py
+++ easy/websec_level-15/websec_level-15.py
@@ -9,4 +9,12 @@ data = {
 }
 
 r = post(url , data=data)
-print r.text
+pos = r.text.find('WEBSEC{')
+while (True):
+    txt = r.text[pos]
+    print(txt , end='')
+    if (txt == '}'):
+        print()
+        break
+
+    pos += 1
